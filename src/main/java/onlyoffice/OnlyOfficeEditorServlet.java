1014dc48700eb54d1f2f5ffcfb286ee932e5dc12
Andrey Panov <gensurkin@yandex.ru>
2019-06-20 15:46:58 +0300

    jwt for full config json

--- src/main/java/onlyoffice/OnlyOfficeEditorServlet.java
+++ src/main/java/onlyoffice/OnlyOfficeEditorServlet.java
@@ -136,33 +136,65 @@ public class OnlyOfficeEditorServlet extends HttpServlet
 		Map<String, String> config = new HashMap<String, String>();
 
 		config.put("docserviceApiUrl", apiUrl + properties.getProperty("files.docservice.url.api"));
-		config.put("callbackUrl", callbackUrl);
-		config.put("fileUrl", fileUrl);
-		config.put("key", key);
-		config.put("fileName", fileName);
 		config.put("errorMessage", errorMessage);
-		config.put("lang", localeManager.getLocale(user).toLanguageTag());
 
-		if (user != null)
-		{
-			config.put("userId", user.getName());
-			config.put("userName", user.getFullName());
-		}
+		JSONObject responseJson = new JSONObject();
+		JSONObject documentObject = new JSONObject();
+		JSONObject editorConfigObject = new JSONObject();
+		JSONObject userObject = new JSONObject();
+		JSONObject permObject = new JSONObject();
 
-		if (jwtManager.jwtEnabled()) {
-			JSONObject responseJson = new JSONObject(config);
-			try {
-				config.put("token", jwtManager.createToken(responseJson));
-			} catch (Exception ex) {
-				StringWriter sw = new StringWriter();
-				PrintWriter pw = new PrintWriter(sw);
-				ex.printStackTrace(pw);
-				String error = ex.toString() + "\n" + sw.toString();
-				log.error(error);
+		String docTitle = fileName.trim();
+		String docExt = docTitle.substring(docTitle.lastIndexOf(".") + 1).trim().toLowerCase();
+
+		try {
+			responseJson.put("type", "desktop");
+			responseJson.put("width", "100%");
+			responseJson.put("height", "100%");
+			responseJson.put("documentType", getDocType(docExt));
+
+			responseJson.put("document", documentObject);
+			documentObject.put("title", docTitle);
+			documentObject.put("url", fileUrl);
+			documentObject.put("fileType", docExt);
+			documentObject.put("key", key);
+			documentObject.put("permissions", permObject);
+			permObject.put("edit", callbackUrl != null && !callbackUrl.isEmpty());
+
+			responseJson.put("editorConfig", editorConfigObject);
+			editorConfigObject.put("lang", localeManager.getLocale(user).toLanguageTag());
+			editorConfigObject.put("mode", "edit");
+			editorConfigObject.put("callbackUrl", callbackUrl);
+
+			if (user != null) {
+				editorConfigObject.put("user", userObject);
+				userObject.put("id", user.getName());
+				userObject.put("firstname", user.getFullName());
+				userObject.put("name", user.getFullName());
 			}
+
+			if (jwtManager.jwtEnabled()) {
+				responseJson.put("token", jwtManager.createToken(responseJson));
+			}
+
+			// AsHtml at the end disables automatic html encoding
+			config.put("jsonAsHtml", responseJson.toString());
+		} catch (Exception ex) {
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			ex.printStackTrace(pw);
+			String error = ex.toString() + "\n" + sw.toString();
+			log.error(error);
 		}
 
 		defaults.putAll(config);
 		return VelocityUtils.getRenderedTemplate("templates/editor.vm", defaults);
 	}
+
+	private String getDocType(String ext) {
+        if (".doc.docx.docm.dot.dotx.dotm.odt.fodt.ott.rtf.txt.html.htm.mht.pdf.djvu.fb2.epub.xps".indexOf(ext) != -1) return "text";
+        if (".xls.xlsx.xlsm.xlt.xltx.xltm.ods.fods.ots.csv".indexOf(ext) != -1) return "spreadsheet";
+        if (".pps.ppsx.ppsm.ppt.pptx.pptm.pot.potx.potm.odp.fodp.otp".indexOf(ext) != -1) return "presentation";
+        return null;
+    }
 }
