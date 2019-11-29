5eba63e1cd86b6a1743538f159aeb5d8c0f0cc85
Andrey Panov <gensurkin@yandex.ru>
2019-11-29 16:14:03 +0300

    convert service

---
+++ src/main/java/onlyoffice/IsOfficeFileConvertAttachment.java
@@ -0,0 +1,39 @@
+package onlyoffice;
+
+import java.util.Map;
+
+import com.atlassian.confluence.pages.Attachment;
+import com.atlassian.confluence.user.AuthenticatedUserThreadLocal;
+import com.atlassian.confluence.user.ConfluenceUser;
+import com.atlassian.plugin.PluginParseException;
+import com.atlassian.plugin.web.Condition;
+
+/*
+    Copyright (c) Ascensio System SIA 2019. All rights reserved.
+    http://www.onlyoffice.com
+*/
+
+public class IsOfficeFileConvertAttachment implements Condition {
+    public void init(Map<String, String> params) throws PluginParseException {
+    }
+
+    public boolean shouldDisplay(Map<String, Object> context) {
+        Attachment attachment = (Attachment) context.get("attachment");
+        if (attachment == null) {
+            return false;
+        }
+        String ext = attachment.getFileExtension();
+        if (attachment.getFileSize() > DocumentManager.GetMaxFileSize()) {
+            return false;
+        }
+
+        ConfluenceUser user = AuthenticatedUserThreadLocal.get();
+        boolean accessEdit = AttachmentUtil.checkAccess(attachment, user, true);
+
+        if (!accessEdit || !ConvertManager.isConvertable(ext)) {
+            return false;
+        }
+
+        return true;
+    }
+}
