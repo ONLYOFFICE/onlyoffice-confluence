dedfeca4f19d484cbdade7ef42a35e9ba4719709
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2021-07-22 22:14:11 +0300

    added Convertible formats

--- src/main/java/onlyoffice/managers/convert/ConvertManagerImpl.java
+++ src/main/java/onlyoffice/managers/convert/ConvertManagerImpl.java
@@ -19,6 +19,8 @@
 package onlyoffice.managers.convert;
 
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -52,18 +54,6 @@ public class ConvertManagerImpl implements ConvertManager {
     private final ConfigurationManager configurationManager;
     private final DocumentManager documentManager;
 
-    @SuppressWarnings("serial")
-    private final Map<String, String> convertableDict = new HashMap<String, String>() {
-        {
-            put("odt", "docx");
-            put("doc", "docx");
-            put("odp", "pptx");
-            put("ppt", "pptx");
-            put("ods", "xlsx");
-            put("xls", "xlsx");
-        }
-    };
-
     @Inject
     public ConvertManagerImpl(UrlManager urlManager, JwtManager jwtManager,
                               ConfigurationManager configurationManager,
@@ -75,11 +65,20 @@ public class ConvertManagerImpl implements ConvertManager {
     }
 
     public boolean isConvertable(String ext) {
-        return convertableDict.containsKey(trimDot(ext));
+        String convertableTypes = configurationManager.getProperty("docservice.type.convert");
+        if(convertableTypes == null) return false;
+        List<String> exts = Arrays.asList(convertableTypes.split("\\|"));
+        return exts.contains(ext);
     }
 
     public String convertsTo(String ext) {
-        return convertableDict.getOrDefault(trimDot(ext), null);
+        String docType = documentManager.getDocType(ext);
+        if (docType != null) {
+            if (docType.equals("text")) return "docx";
+            if (docType.equals("spreadsheet")) return "xlsx";
+            if (docType.equals("presentation")) return "pptx";
+        }
+        return null;
     }
 
     public JSONObject convert(Long attachmentId, String ext) throws Exception {
