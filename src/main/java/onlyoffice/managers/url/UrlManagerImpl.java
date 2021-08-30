14dd8805e92fa57c35d83477ff3260383e1fe1f7
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2021-08-30 18:51:09 +0300

    fixed typo callbackServlet

--- src/main/java/onlyoffice/managers/url/UrlManagerImpl.java
+++ src/main/java/onlyoffice/managers/url/UrlManagerImpl.java
@@ -43,7 +43,7 @@ import javax.servlet.http.HttpServletRequest;
 @Default
 public class UrlManagerImpl implements UrlManager {
     private final Logger log = LogManager.getLogger("onlyoffice.managers.url.UrlManager");
-    private final String callbackServler = "plugins/servlet/onlyoffice/save";
+    private final String callbackServlet = "plugins/servlet/onlyoffice/save";
     private final String historyServlet = "plugins/servlet/onlyoffice/history";
     private final String fileProviderServlet = "plugins/servlet/onlyoffice/file-provider";
 
@@ -125,7 +125,7 @@ public class UrlManagerImpl implements UrlManager {
     public String getCallbackUrl(Long attachmentId) {
         String hash = documentManager.createHash(Long.toString(attachmentId));
 
-        String callbackUrl = getConfluenceBaseUrl() + callbackServler + "?vkey=" + GeneralUtil.urlEncode(hash);
+        String callbackUrl = getConfluenceBaseUrl() + callbackServlet + "?vkey=" + GeneralUtil.urlEncode(hash);
         log.info("callbackUrl " + callbackUrl);
 
         return callbackUrl;
