b0e92aee616a7b822a744e5516a3492584b318ca
VGKurguzov <vladimir.kurguzov@onlyoffice.com>
2023-01-13 15:25:12 +0300

    feature: add error handling

--- src/main/resources/js/onlyoffice-download-as-popup.js
+++ src/main/resources/js/onlyoffice-download-as-popup.js
@@ -7,6 +7,7 @@
             AJS.$.get(link.attr('href'), function (response) {
                 AJS.$('.aui-page-panel').after(response);
                 AJS.dialog2("#download-as-popup").show();
+                $(".aui-message-context").html("");
                 $("#download-as-popup-binder").css("padding-bottom", "0");
 
                 AJS.$('#dialog-close-button').bind("click", function (e) {
@@ -42,17 +43,70 @@
             type: "POST",
             cache: false,
             success: function (data) { onResponse(data) },
-            error: function (data) { console.log(data) }
+            error: function (data) { onError(data) }
         });
     }
+    function getErrorByResponse(response) {
+        if (response.errorData && response.errorData.error) {
+            var errorMessage;
+            var servicePrefix = false;
 
-    function onError(error) {
-        console.log(error);
+            switch (response.errorData.error) {
+                case -1:
+                    errorMessage = AJS.I18n.getText("onlyoffice.connector.dialog.conversion.message.error.unknown");
+                    servicePrefix = true;
+                    break;
+                case -2:
+                    errorMessage = AJS.I18n.getText("onlyoffice.connector.dialog.conversion.message.error.timeout");
+                    servicePrefix = true;
+                    break;
+                case -3:
+                    errorMessage = AJS.I18n.getText("onlyoffice.connector.dialog.conversion.message.error.conversion");
+                    servicePrefix = true;
+                    break;
+                case -4:
+                    errorMessage = AJS.I18n.getText("onlyoffice.connector.dialog.conversion.message.error.download");
+                    servicePrefix = true;
+                    break;
+                case -5:
+                    errorMessage = AJS.I18n.getText("onlyoffice.connector.dialog.conversion.message.error.password");
+                    servicePrefix = true;
+                    break;
+                case -6:
+                    errorMessage = AJS.I18n.getText("onlyoffice.connector.dialog.conversion.message.error.database");
+                    servicePrefix = true;
+                    break;
+                case -7:
+                    errorMessage = AJS.I18n.getText("onlyoffice.connector.dialog.conversion.message.error.input");
+                    servicePrefix = true;
+                    break;
+                case -8:
+                    errorMessage = AJS.I18n.getText("onlyoffice.connector.dialog.conversion.message.error.token");
+                    servicePrefix = true;
+                    break;
+                case -10:
+                    errorMessage = AJS.I18n.getText("onlyoffice.connector.dialog.conversion.message.error.not-reached");
+                    break;
+                default:
+                    errorMessage = AJS.I18n.getText("onlyoffice.connector.error.Unknown");
+            }
+
+            if (servicePrefix) {
+                errorMessage = AJS.I18n.getText("onlyoffice.connector.dialog.conversion.message.error.service-prefix").replace("$", errorMessage);
+            }
+
+            return errorMessage;
+        }
+    }
+
+    function onError(response) {
+        var errorMessage = getErrorByResponse(response);
+        $(".aui-message-context").html("<div class='aui-message aui-message-error'>" + errorMessage + "</div>");
     }
 
     function onResponse(data) {
-        if (data.error) {
-            onError(data.error);
+        if (data.errorData) {
+            onError(data);
             return;
         }
 
