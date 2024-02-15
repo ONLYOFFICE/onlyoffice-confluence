8e15d12a4b2c9819e6e46141f4f901bdfbd49a14
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-02-15 16:37:21 +0300

    feat: added the ability to open documents for viewing

--- src/main/java/onlyoffice/OnlyOfficeEditorServlet.java
+++ src/main/java/onlyoffice/OnlyOfficeEditorServlet.java
@@ -88,6 +88,7 @@ public class OnlyOfficeEditorServlet extends HttpServlet {
 
         String attachmentIdString = request.getParameter("attachmentId");
         String actionDataString = request.getParameter("actionData");
+        String modeString = request.getParameter("mode");
         if (attachmentIdString == null || attachmentIdString.isEmpty()) {
             if (!authContext.checkUserAuthorization(request, response)) {
                 return;
@@ -165,9 +166,15 @@ public class OnlyOfficeEditorServlet extends HttpServlet {
                     actionData = new JSONObject(actionDataString);
                 }
 
+                Mode mode = Mode.EDIT;
+
+                if (modeString != null && modeString.equals("view")) {
+                    mode = Mode.VIEW;
+                }
+
                 Config config = configService.createConfig(
                         attachmentId.toString(),
-                        Mode.EDIT,
+                        mode,
                         request.getHeader("USER-AGENT")
                 );
 
